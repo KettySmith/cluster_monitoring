813353079654710236792c7e3e74cc0dd9eae08d
wuyujia <351681533@qq.com>
2023-06-04 13:32:23 +0800

    改进波动相似度分析算法

--- soft_backend/soft/search/search_data.py
+++ soft_backend/soft/search/search_data.py
@@ -7,7 +7,7 @@ from soft.sql.connect_pool import *
 
 
 # 相似度比对
-@search_blue.route("/similarity_search", methods=['POST'])
+# @search_blue.route("/similarity_search", methods=['POST'])
 def similarity_search():
     # input_json = request.form(force=True)
     # print(input_json)
@@ -38,12 +38,16 @@ def process_similarity(rank: int, ab_node: str, start_time: str, end_time: str):
     ret = get_similarity_data(nodes_name, ab_node, start_time, end_time, cursor)
     nodes_name.remove(ab_node)
     query = ret.pop(ab_node)
+    # normalize
+    query['y_data_list'] = list(map(lambda y: (y-query['y_min']) / (query['y_max']-query['y_min']), query['y_data_list']))  
     refers = ret
 
     simi_list = []
     while (len(refers)):
         curr_node = nodes_name.pop()
         refer = refers.pop(curr_node)
+        # normalize
+        refer['y_data_list'] = list(map(lambda y: (y-refer['y_min']) / (refer['y_max']-refer['y_min']), refer['y_data_list']))  
         # dtw算法对比两个序列的相似度
         alignment = dtw(x=query['y_data_list'],
                         y=refer['y_data_list'],
@@ -65,6 +69,7 @@ def process_similarity(rank: int, ab_node: str, start_time: str, end_time: str):
     # 0
     node_simi = dict()
     node_simi['node_name'] = "abnormal0"
+    node_simi['simi_value'] = 0
     node_simi['x_data_list'] = simi_list[0]['x_data_list']
 
     y_data_list = [-1, -1]
@@ -74,6 +79,9 @@ def process_similarity(rank: int, ab_node: str, start_time: str, end_time: str):
     simi_list.append(node_simi)
 
     sql_end(con, cursor)
+
+    for simi in simi_list:
+        print(simi['node_name'], simi['simi_value'])
     return simi_list
 
 
@@ -103,6 +111,8 @@ def get_similarity_data(nodes_name: list, ab_node: str, start_time: str, end_tim
         datas = select_node_similarity_data(node_name, ab_node == node_name, start_time, end_time, cursor)
         res[node_name]['x_data_list'] = datas[0]
         res[node_name]['y_data_list'] = datas[1]
+        res[node_name]['y_min'] = datas[2]
+        res[node_name]['y_max'] = datas[3]
     return res
 
 
@@ -119,6 +129,9 @@ def select_node_similarity_data(table_name: str, isQuery: bool, start_time: str,
     datas = cursor.fetchall()
     x_data_list = []
     y_data_list = []
+    # normalize
+    y_min = float('inf')
+    y_max = float(0)
     # 对于对比序列，考虑波动提前/延后的情况，将对比时间段边界放宽
     for data in datas:
         if (not isQuery and data[0] >= start_id - 2 and data[0] < start_id):
@@ -132,8 +145,14 @@ def select_node_similarity_data(table_name: str, isQuery: bool, start_time: str,
             y_data_list.append(float(data[2]))
         elif (data[0] > end_id + 2):
             break
-    return x_data_list, y_data_list
+        # normalize
+        if(float(data[2]) > y_max): y_max = float(data[2])
+        if(float(data[2]) < y_min): y_min = float(data[2])
+    return x_data_list, y_data_list, y_min, y_max
+
+
+
 
 
 if __name__ == '__main__':
-    process_similarity(5, 'abnormal0', '2023/4/23 15:20', '2023/4/23 17:20')
+    process_similarity(20, 'abnormal0', '2023/4/23 15:20', '2023/4/23 17:20')
